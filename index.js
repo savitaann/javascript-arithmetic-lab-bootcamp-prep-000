4f44fb2f25b033f47eb55ead9b4ca89fccb36e48
savita madray <savitamadray01@gmail.com>
2017-09-29 18:31:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,42 @@
+function add(a, b) {
+  return a + b
+}
+
+add(5, 6)
+
+function subtract(a, b) {
+  return a-b
+}
+
+subtract(10, 5)
+
+function multiply(a, b) {
+  return a*b
+}
+
+multiply(5,5)
+
+function divide(a, b) {
+  return a/b
+}
+
+divide(8, 2)
+
+function inc(n) {
+    return n + 1
+}
+
+inc(6)
+
+function dec(n) {
+  n--
+ return n
+}
+
+dec(7)
+
+function makeInt(n){
+  return parseInt('n',  10)
+}
+
+makeInt('15')
